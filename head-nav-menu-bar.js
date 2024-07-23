5843425e27ff03b2a0042efae9dcbcff9a351d22
jeppp11 <jepagador11@gmail.com>
2024-07-23 19:25:45 +0800

    "edited file names and added nav-bar js"

---
+++ head-nav-menu-bar.js
@@ -0,0 +1,19 @@
+export default function NavMenuBar() {
+    return <nav className="nav-menu-bar">
+        <a href="/" className="meco-name"> Mecosono</a>
+        <ul>
+            <li>
+                <a href="/About">About Mecosono</a>
+            </li>
+            <li>
+                <a href="/Students">Students</a>
+            </li>
+            <li>
+                <a href="/Contact">Contact Us</a>
+            </li>
+            <li>
+                <a href="Store">Store</a>
+            </li>
+        </ul>
+    </nav>
+}
